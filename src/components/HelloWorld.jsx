60a7dc7203da673e3288618a62ee49812601dbfe
Sebastian Gomez <gomeztabaresjuansebastian@gmail.com>
2020-09-17 16:10:50 -0500

    a initial development environmnet

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const HelloWorld = () => (
+
+  <h2>Holii</h2>
+);
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helloworld from './components/HelloWorld';
+
+ReactDOM.render(<Helloworld />, document.getElementById('app'));
+//lo que esta realziando es buscar el archivo Helloworld.jsx y renderizarlo en la etiqueta de html con id ‘app’
