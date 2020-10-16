f3a877bbb7d82708b8b78c97790674643e170384
Sebastian Gomez <gomeztabaresjuansebastian@gmail.com>
2020-10-15 21:23:21 -0500

    putting the logo on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moon from '../assets/static/moon.svg';
 import '../assets/styles/Header.scss';
+import logo from '../assets/static/logo.png';
 
 const Header = () => (
   <div className='header'>
     <Link to='/' style={{ textDecoration: 'none' }}>
       <h2 className='logoTitle'>Where in the world?</h2>
     </Link>
+    <img src={logo} className='logoImage' alt=""/>
     <div className='switch'>
       <img src={moon} alt='' />
       <h3>Dark Mode</h3>
